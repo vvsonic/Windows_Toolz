de348b7bcd4d1dc06a7da681894c46e3e4cf2300
Sonic Systems <73136664+vvsonic@users.noreply.github.com>
2025-02-14 23:26:09 -0800

    Update Main-Stager.ps1
    Removed App Updates as we have a separate script that does this for us. Set Windows updates to run first.

--- Production/Curl-able/Windows_Debloat/Main-Stager.ps1
+++ Production/Curl-able/Windows_Debloat/Main-Stager.ps1
@@ -15,23 +15,6 @@ Write-Host "  Created by Alex Ivantsov "
 Write-Host "  @Exploitacious "
 
 Write-Host
-Write-Host
-Write-Host
-Write-Host
-
-# Check for Files Here
-# Required Files (xyz...)
-
-
-Write-Host "Launching De-Bloat Processes..."
-Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"C:\Temp\Cleanup\UninstallBloat.ps1`"" -Verb RunAs
-
-$i = 5 #Seconds
-do {
-    Write-Host $i
-    Sleep 1
-    $i--
-} while ($i -gt 0)
 
 Write-Host "Launching PS Modules & Windows Updates"
 Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"C:\Temp\Cleanup\PSandWindowsUpdates.ps1`"" -Verb RunAs
@@ -43,10 +26,10 @@ do {
     $i--
 } while ($i -gt 0)
 
-Write-Host "Launching Winget Auto Update and New App Installations"
-Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"C:\Temp\Cleanup\InstallNewApps.ps1`"" -Verb RunAs
+Write-Host "Launching De-Bloat Processes..."
+Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"C:\Temp\Cleanup\UninstallBloat.ps1`"" -Verb RunAs
 
-$i = 60 #Seconds
+$i = 5 #Seconds
 do {
     Write-Host $i
     Sleep 1
@@ -58,23 +41,3 @@ Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"C:\Temp
 
 
 Read-Host -Prompt "Finished! Press Enter to exit"
-
-<#
-
-# Implement User Logon Script
-
-Write-Host "Creating Directories 'C:\Windows\FirstUserLogon' and Copying files"
-mkdir "C:\Windows\FirstUserLogon" -ErrorAction SilentlyContinue
-Copy-Item "DebloatScript-HKCU.ps1" "C:\Windows\FirstUserLogon\DebloatScript-HKCU.ps1"
-Copy-Item "FirstLogon.bat" "C:\Windows\FirstUserLogon\FirstLogon.bat"
-Write-Host
-
-Write-Host "Enabling Registry Keys to run Logon Script"
-REG LOAD HKEY_Users\DefaultUser "C:\Users\Default\NTUSER.DAT"
-Set-ItemProperty -Path "REGISTRY::HKEY_USERS\DefaultUser\Software\Microsoft\Windows\CurrentVersion\Run" -Name "FirstUserLogon" -Value "C:\Windows\FirstUserLogon\FirstLogon.bat" -Type "String"
-REG UNLOAD HKEY_Users\DefaultUser
-	
-Write-Host "New User Logon Script Successfully Enabled"
-
-
-#>
