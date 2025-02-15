e9a8611dee39094e20c624dde0313746a06fac8d
Sonic Systems <73136664+vvsonic@users.noreply.github.com>
2025-02-15 00:18:41 -0800

    Update UninstallBloat.ps1
    Explicit exit to the script

--- Production/Curl-able/Windows_Debloat/UninstallBloat.ps1
+++ Production/Curl-able/Windows_Debloat/UninstallBloat.ps1
@@ -212,4 +212,6 @@ foreach ($MsiBloat in $MsiBloatList ) {
 }
 
 Write-Host
-Read-Host -Prompt "Finished! Press Enter to exit"
+#Read-Host -Prompt "Finished! Press Enter to exit"
+# Explicitly exit after completion to ensure the script terminates
+exit
